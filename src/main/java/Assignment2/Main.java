37349b470eeb514e523aa4eb3f017a883e6013cf
monitosh <xvp5413@aut.ac.nz>
2022-06-01 18:57:07 +1200

    test commit

---
+++ src/main/java/Assignment2/Main.java
@@ -0,0 +1,13 @@
+package Assignment2;
+
+/**
+ * @author Monitosh Thaker | 17000777
+ * COMP603 Assignment 2
+ */
+public class Main 
+{
+    public static void main(String[] args) 
+    {        
+        System.out.println("hello world");
+    }
+}
