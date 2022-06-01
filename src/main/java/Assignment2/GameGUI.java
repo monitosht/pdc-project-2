b1dec9eea1f4d3c993a4210526094b7b43b2d194
monitosh <xvp5413@aut.ac.nz>
2022-06-01 19:24:39 +1200

    created JFrame
    Created the initial game window by using a JFrame variable.

--- src/main/java/Assignment2/GameGUI.java
+++ src/main/java/Assignment2/GameGUI.java
@@ -4,7 +4,20 @@ package Assignment2;
  * @author Monitosh Thaker | 17000777
  * COMP603 Assignment 2
  */
+import javax.swing.*;
+
 public class GameGUI 
 {
+    //Main game window
+    public static JFrame gameWindow;
     
+    public static void startScene()
+    {
+        gameWindow = new JFrame();
+        gameWindow.setTitle("Moni's RPG Adventure");
+        gameWindow.setSize(1280,720);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setResizable(false);
+        gameWindow.setVisible(true);
+    }    
 }
--- src/main/java/Assignment2/Main.java
+++ src/main/java/Assignment2/Main.java
@@ -7,7 +7,7 @@ package Assignment2;
 public class Main 
 {
     public static void main(String[] args) 
-    {        
-        System.out.println("hello world");
+    {   
+        GameGUI.startScene();
     }
 }
