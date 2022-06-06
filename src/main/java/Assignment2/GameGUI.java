084cd318a8b04df61b3a4ad8169be42b7566db46
monitosh <xvp5413@aut.ac.nz>
2022-06-06 22:32:04 +1200

    Starting Main Menu UI
    Starting to add basic main menu UI elements.

--- src/main/java/Assignment2/GameGUI.java
+++ src/main/java/Assignment2/GameGUI.java
@@ -4,21 +4,115 @@ package Assignment2;
  * @author Monitosh Thaker | 17000777
  * COMP603 Assignment 2
  */
+import java.awt.*;
 import javax.swing.*;
 
 public class GameGUI 
 { 
-    //Main game window
-    public static JFrame gameWindow;
+    //global
+    static Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    static Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
     
-    public static void startScene()
+    
+    //game window
+    static JFrame gameWindow;
+    
+    //main menu
+    static JPanel titlePanel;
+    static JLabel titleLabel;
+    
+    static JPanel newGamePanel;
+    static JButton newGameButton;
+    
+    static JPanel continuePanel;
+    static JButton continueButton;
+    
+    static JPanel creditsPanel;
+    static JButton creditsButton;
+    
+    static JPanel quitPanel;
+    static JButton quitButton;
+    
+    
+    public static void frameSetup()
     {
         gameWindow = new JFrame();
         gameWindow.setTitle("Moni's RPG Adventure");
         gameWindow.setSize(1280,800);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.setLocationRelativeTo(null); //center window
+        gameWindow.setLocationRelativeTo(null); //center window when opened
         gameWindow.setResizable(false);
         gameWindow.setVisible(true);
-    }    
+    }
+    
+    public static void mainMenu()
+    {   
+        //TITLE
+        titlePanel = new JPanel();
+        titlePanel.setBounds(340,150,600,150);
+        titlePanel.setBackground(Color.white);
+        
+        titleLabel = new JLabel("RPG Adventure");
+        titleLabel.setForeground(Color.black);
+        titleLabel.setFont(titleFont);
+        
+        titlePanel.add(titleLabel);
+        
+        //NEW GAME
+        newGamePanel = new JPanel();
+        newGamePanel.setBounds(560,375,160,40);
+        newGamePanel.setBackground(Color.white);
+        
+        newGameButton = new JButton("New Game");
+        newGameButton.setBackground(Color.white);
+        newGameButton.setForeground(Color.black);
+        newGameButton.setFont(normalFont);
+        
+        newGamePanel.add(newGameButton);
+        
+        //CONTINUE
+        continuePanel = new JPanel();
+        continuePanel.setBounds(560,450,160,40);
+        continuePanel.setBackground(Color.white);
+        
+        continueButton = new JButton("Continue");
+        continueButton.setBackground(Color.white);
+        continueButton.setForeground(Color.black);
+        continueButton.setFont(normalFont);
+        
+        continuePanel.add(continueButton);
+        
+        //CREDITS
+        creditsPanel = new JPanel();
+        creditsPanel.setBounds(560,525,160,40);
+        creditsPanel.setBackground(Color.white);
+        
+        creditsButton = new JButton("Credits");
+        creditsButton.setBackground(Color.white);
+        creditsButton.setForeground(Color.black);
+        creditsButton.setFont(normalFont);
+        
+        creditsPanel.add(creditsButton);      
+        
+        //QUIT
+        quitPanel = new JPanel();
+        quitPanel.setBounds(560,600,160,40);
+        quitPanel.setBackground(Color.white);
+        
+        quitButton = new JButton("123");
+        quitButton.setBackground(Color.white);
+        quitButton.setForeground(Color.black);
+        quitButton.setFont(normalFont);
+        
+        quitPanel.add(quitButton);
+        
+        //add to game window
+        gameWindow.add(titlePanel);        
+        gameWindow.add(newGamePanel);
+        gameWindow.add(continuePanel);
+        gameWindow.add(creditsPanel);
+        gameWindow.add(quitPanel);
+        gameWindow.revalidate();
+        gameWindow.repaint();
+    }
 }
--- src/main/java/Assignment2/TestGUI.java
+++ src/main/java/Assignment2/TestGUI.java
@@ -24,25 +24,80 @@ public class TestGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Moni's RPG Adventure");
         setLocation(new java.awt.Point(100, 100));
         setResizable(false);
         setSize(new java.awt.Dimension(1280, 800));
 
+        jButton1.setText("Quit");
+
+        jButton2.setText("Main Menu");
+
+        jButton3.setText("Continue");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Credits");
+        jButton4.setToolTipText("");
+
+        jButton5.setText("New Game");
+
+        jButton6.setText("Quit");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 880, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(370, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(350, 350, 350))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 420, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 166, Short.MAX_VALUE)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +135,11 @@ public class TestGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     // End of variables declaration//GEN-END:variables
 }
